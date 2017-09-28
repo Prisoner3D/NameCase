5e1e32b83027dcf97c4984bc36758e7d29a50715
Teacher <Teacher@10.8.39.80>
2017-09-28 12:16:59 -0400

    Initialized repository with starter files.

---
+++ Lab1.2/src/NLevinNameCase.java
@@ -0,0 +1,23 @@
+ /**
+ * Created by Mr.Levin  on 9/28/17.
+ */
+public class NLevinNameCase implements NameCase {
+    public static void main (String [] args)
+    {
+    	//test your code here
+    }
+
+    public String titlecase(String name)
+    {
+        return "";
+    }
+    public String swapcase(String name)
+    {
+        return "";
+    }
+    public String alternatingcase(String name)
+    {
+        return "";
+    }
+
+}
